cfbe87e11c92bca413629937078c6272beb917cc
Abdisa Gemechu <myabdisa@gmail.com>
2021-10-05 04:11:30 -0700

    added first task

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAINH
+#define MAINH
+
+#include <stdarg.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_rev(va_list r);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
+/**
+  * struct code_format - Struct format
+  *
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
+
+#endif /* HOLBERTONH */
