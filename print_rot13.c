31a43f740eaf7fd0a53c8c934fd78f316a8968b9
yaredow <yaredyilma11@gmail.com>
2021-10-06 10:13:04 -0700

    Revert "rot13r"
    This reverts commit e21f73337809d81fab81ba3384c4d6a6008515f2.

--- print_rot13.c
+++ print_rot13.c
@@ -1,60 +1,32 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_rot13 - encodes a string using rot13 encryption
- * @s: string to be encoded
- *
- * Return: number of chars printed
- */
-int print_rot13(char *s)
+  * print_rot13 - encodes a string into rot13.
+  * @R: string to convert
+  * Return: size the output text
+  */
+int print_rot13(va_list R)
 {
-	char nor[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		      'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		      'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-		      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-		      'Z', ' '};
-	char rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		      'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		      'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-		      'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-		      'M', ' '};
-	int count, len, count2 = 0, chars_printed = 0;
-	char now, *rot13d;
+	int j, i, count = 0;
+	char *r;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLM nopqrstuvwxyzabcdefghijklm";
 
-	len = 0;
-	while (*(s + len))
-		len++;
-	rot13d = malloc(sizeof(char) * len);
-	if (rot13d == NULL)
+	r = va_arg(R, char *);
+	if (r == NULL)
+		r = "(null)";
+	for (j = 0; r[j] != '\0'; j++)
 	{
-		free(rot13d);
-		return (0);
-	}
-	len = 0;
-	while (*(s + len))
-	{
-		now = *(s + len);
-		for (count = 0; count < 53; count++)
+		for (i = 0; input[i] != '\0'; i++)
 		{
-			if (now == nor[count])
+			if (r[j] == input[i])
 			{
-				*(rot13d + len) = rot[count];
-				count2 = count;
+				_putchar(output[i]);
+				count++;
 				break;
 			}
 		}
-		if (*(rot13d + len) != rot[count2])
-			*(rot13d + len) = now;
-		if (*(rot13d + len) == '\\')
-		{
-			*(rot13d + len + 1) = *(s + len + 1);
-			len++;
-		}
-		len++;
 	}
-	*(rot13d + len) = '\0';
-	chars_printed = print_string(rot13d);
-	free(rot13d);
-	return (chars_printed);
+	return (count);
 }
 
