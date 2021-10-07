e507efb2fb5425e5bc3a8a6b40bf9912899f6016
Abdisa Gemechu <myabdisa@gmail.com>
2021-10-07 08:46:28 -0700

    added some changes

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -1,11 +1,6 @@
 #include "main.h"
-
-
-
 unsigned int convert_c(va_list args, buffer_t *output,
-		       
-		       unsigned char flags, int wid, int prec, unsigned char len);
-
+unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_percent(va_list args, buffer_t *output,
 			     
 			     unsigned char flags, int wid, int prec, unsigned char len);
--- handlers.c
+++ handlers.c
@@ -1,42 +1,22 @@
 #include "main.h"
-
-
-
 unsigned char handle_flags(const char *flag, char *index);
-
 unsigned char handle_length(const char *modifier, char *index);
-
 int handle_width(va_list args, const char *modifier, char *index);
-
 int handle_precision(va_list args, const char *modifier, char *index);
-
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-							 
-							 unsigned char, int, int, unsigned char);
-
-
-
+unsigned char, int, int, unsigned char);
 /**
-
  * handle_flags - Matches flags with corresponding values.
-
  * @flag: A pointer to a potential string of flags.
-
  * @index: An index counter for the original format string.
-
  *
-
  * Return: If flag characters are matched - a corresponding value.
-
  *         Otherwise - 0.
-
  */
-
 unsigned char handle_flags(const char *flag, char *index)
-  
 {
   
-  int i, j;
+int i, j;
   
   unsigned char ret = 0;
   
